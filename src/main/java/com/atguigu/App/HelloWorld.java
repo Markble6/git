a52bca56b265ff9ad9c81303ba977cd63c5f655b
Mark <13262817131@163.com>
2020-10-09 23:15:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/App/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.App;
+
+/**
+ * @author Mark
+ * @create 2020-10-09 23:11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello java");
+        System.out.println("hello spark");
+    }
+}
